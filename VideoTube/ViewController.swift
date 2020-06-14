0f60a5d066421e655dffc86b4f8d8daa0d1cee6c
athakur <athakur@sgintl.com>
2020-06-14 07:59:03 +0530

    initial xcode project creation

---
+++ VideoTube/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoTube
+//
+//  Created by Amit Thakur on 14/06/20.
+//  Copyright © 2020 technogenLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
